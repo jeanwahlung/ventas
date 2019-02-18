1d3bf2be26319b1e1383928df84c407390be4bcc
jeanwahlung <jeancarloswahlung@gmail.com>
2019-02-18 12:07:34 -0600

    Updates

---
+++ precache-manifest.d426733ef07c8178bfe52062e6cad10a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3aeeab52aba0daaf784",
+    "url": "/Examen1UX/static/js/runtime~main.b3aeeab5.js"
+  },
+  {
+    "revision": "db5d9b771446b37b44c6",
+    "url": "/Examen1UX/static/js/main.db5d9b77.chunk.js"
+  },
+  {
+    "revision": "513dec50a98ee8b885a7",
+    "url": "/Examen1UX/static/js/2.513dec50.chunk.js"
+  },
+  {
+    "revision": "db5d9b771446b37b44c6",
+    "url": "/Examen1UX/static/css/main.5b1870a4.chunk.css"
+  },
+  {
+    "revision": "513dec50a98ee8b885a7",
+    "url": "/Examen1UX/static/css/2.d011e576.chunk.css"
+  },
+  {
+    "revision": "0bd2a2bb0a30a61cedea8e1582466cb0",
+    "url": "/Examen1UX/index.html"
+  }
+];
